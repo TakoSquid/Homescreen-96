e8974c4f3fd2d3e9cad465fa00ca5872a11f02bc
TakoSquid <31723113+TakoSquid@users.noreply.github.com>
2017-09-07 12:39:02 +0200

    Last edits
    Will add actual audio and weather system someday.

--- 96-2/script.js
+++ 96-2/script.js
@@ -1,4 +1,7 @@
 
+var songMinutes = 4;
+var songSeconds = 38;
+var min, sec, displaySec = "";
 
 window.onload = main;
 
@@ -9,9 +12,15 @@ window.setInterval(function(){
 var months = ["Jan", "Fev", "mars", "Avr", "Mai", "Jun", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"]
 var d;
 function main() {
-  date();
+  min = Math.floor(Math.random()*songMinutes);
+  sec = Math.floor(Math.random()*songSeconds);
+  if(sec < 10)
+    displaySec += "0";
+  displaySec += sec;
 
-  document.getElementById("volume").innerHTML = "Volume: " + Math.floor(Math.random()*10);
+  date();
+  fillHeader();
+  progressBar();
 }
 
 
@@ -21,4 +30,17 @@ function date() {
 
   document.getElementById("time").innerHTML = d.getHours() + ":" + d.getMinutes();
   document.getElementById("date").innerHTML = d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+}
+
+function fillHeader() {
+  document.getElementById("artist").innerHTML = "Porter Robinson";
+  document.getElementById("songTitle").innerHTML = "Flicker";
+  document.getElementById("albumName").innerHTML = "Worlds";
+  document.getElementById("timeCode").innerHTML = min + ":" + displaySec;
+  document.getElementById("state").innerHTML = "paused";
+}
+
+function progressBar() {
+  var pourcent = (min * 60 + sec) * 100 / (songMinutes * 60 + songSeconds);
+  document.getElementById("progressBar").style.width = pourcent + "%";
 }
